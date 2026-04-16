bd9dfbf32f1e4ff82b49396e0be90cc91f1c8180
iCommunityR <erone47@gmail.com>
2026-04-16 19:57:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from '@/contexts/AppContext';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <AppProvider>
+    <App />
+  </AppProvider>
+);
+
 // Remove dark mode class addition
 createRoot(document.getElementById("root")!).render(<App />);
