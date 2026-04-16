f004d5da5685972e00a50561300fed67e9b48e8d
iCommunityR <erone47@gmail.com>
2026-04-16 20:56:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+window.onerror = function (message, source, lineno, colno, error) {
+  console.error('GLOBAL ERROR:', { message, source, lineno, colno, error });
+};
+
+window.onunhandledrejection = function (event) {
+  console.error('UNHANDLED PROMISE:', event.reason);
+};
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
