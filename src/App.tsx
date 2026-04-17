037cca0f4af8ff2ccd9fd08e3816ee034ee705d6
iCommunityR <erone47@gmail.com>
2026-04-17 09:47:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { useAppContext } from "@/contexts/AppContext";
+import LoginModal from "@/components/LoginModal";
+import Dashboard from "@/components/Dashboard";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <ThemeProvider defaultTheme="light">
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ThemeProvider>
-);
+const App = () => {
+  const { user } = useAppContext();
+
+  console.log("USER:", user); // debug
+
+  return (
+    <ThemeProvider defaultTheme="light">
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+
+          {!user ? (
+            <LoginModal
+              isOpen={true}
+              onClose={() => {}}
+              onLogin={() => {}}
+              mode="login"
+            />
+          ) : (
+            <Dashboard />
+          )}
+
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
