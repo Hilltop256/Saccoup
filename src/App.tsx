919b9e4795021f9ca5964fb06a8f8a107c314c28
iCommunityR <erone47@gmail.com>
2026-04-17 09:57:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,29 @@
-import { useAppContext } from "../contexts/AppContext";
-import LoginModal from "../components/LoginModal";
-import Dashboard from "../components/Dashboard";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import { useAppContext } from "@/contexts/AppContext";
-import LoginModal from "@/components/LoginModal";
-import Dashboard from "@/components/Dashboard";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  const { user } = useAppContext();
-
-  console.log("USER:", user); // debug
-
-  return (
-    <ThemeProvider defaultTheme="light">
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-
-          {!user ? (
-            <LoginModal
-              isOpen={true}
-              onClose={() => {}}
-              onLogin={() => {}}
-              mode="login"
-            />
-          ) : (
-            <Dashboard />
-          )}
-
-        </TooltipProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider defaultTheme="light">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
+);
 
 export default App;
