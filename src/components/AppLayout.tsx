5b5da8506ffb712457d667152e0cc7b3cf431985
iCommunityR <erone47@gmail.com>
2026-04-24 09:35:20 +0300

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -19,7 +19,7 @@ import MigrationPage from './saccoUp/MigrationPage';
 import SavingsDashboard from './saccoUp/SavingsDashboard';
 import SpreadsheetPage from './saccoUp/SpreadsheetPage';
 
-import KycModal from './KycModal';
+import KycModal from './saccoUp/KycModal';
 
 const { needsKyc } = useAppContext();
 
